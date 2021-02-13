448d9788b1dd4e4862a122c880c61e87d3e8e186
RoryMusinskas <devrorym@gmail.com>
2021-02-13 14:06:38 +1100

    Changed structure of app to have an index page which now renders all components, this is more modular if we were to add more pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import type {} from "styled-components/cssprop";
-import styled from "styled-components";
-import { FavouritesSection } from "./components/Organisms/FavouritesSection";
-import { MainSection } from "./components/Organisms/MainSection";
-
-// styles for the wrapper for the app
-const Wrapper = styled.section`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: papayawhip;
-`;
-
-// setting the type of the favourites from the main section
-type Favourites = {
-  title: string;
-  url: string;
-};
+import { Index } from "./components/pages/Index";
 
 const App: React.FC = () => {
-  // setting the state, the type is an array of objects with the shape of the favourites type
-  const [favourites, setFavourites] = useState<Favourites[]>([]);
-
-  // return the favourites and main sections
-  return (
-    <Wrapper>
-      <FavouritesSection favourites={favourites} />
-      <MainSection favourites={favourites} setFavourites={setFavourites} />
-    </Wrapper>
-  );
+  return <Index />;
 };
 
 export default App;
