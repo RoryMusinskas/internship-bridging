0e577e8b7bc51330c607540e218de49a90a51400
RoryMusinskas <devrorym@gmail.com>
2021-02-13 13:42:23 +1100

    Completed the basic integration of the app component. Will restructure to try remove state from here.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import type {} from 'styled-components/cssprop'
-import styled from 'styled-components'
-import { TextField } from './components/TextField';
+import React, { useState } from "react";
+import type {} from "styled-components/cssprop";
+import styled from "styled-components";
+import { FavouritesSection } from "./components/Organisms/FavouritesSection";
+import { MainSection } from "./components/Organisms/MainSection";
 
-const App: React.FC = () => {
-  const Wrapper = styled.section`
+// styles for the wrapper for the app
+const Wrapper = styled.section`
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   background: papayawhip;
-  `;
+`;
 
+// setting the type of the favourites from the main section
+type Favourites = {
+  title: string;
+  url: string;
+};
+
+const App: React.FC = () => {
+  // setting the state, the type is an array of objects with the shape of the favourites type
+  const [favourites, setFavourites] = useState<Favourites[]>([]);
+
+  // return the favourites and main sections
   return (
     <Wrapper>
-    <TextField text='Typescript is cool'/>
+      <FavouritesSection favourites={favourites} />
+      <MainSection favourites={favourites} setFavourites={setFavourites} />
     </Wrapper>
-    );
-}
+  );
+};
 
 export default App;
